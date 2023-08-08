4f9d9c3d65953331b78b259df2c7bbed81f7154d
Tom Joshi-Cale <tj294@exeter.ac.uk>
2023-08-08 16:18:07 +0100

    Fixed bug in League creation

--- API/league.py
+++ API/league.py
@@ -10,24 +10,27 @@ for file in glob('../Teams/*'):
 
 local_rivals = {
     "Parliamentary Penpushers": "Brighton Beachcombers",
-    "Brighton Beachcombers": "Parliamentary Penpushers",
     "Birmingham Bullfrogs": "Nottingham Nightingales",
-    "Nottingham Nightingales": "Birmingham Bullfrogs",
     "Edinburgh XI": "Glasgow Goofballs",
-    "Glasgow Goofballs": "Edinburgh XI",
     "Bristol Bats": "Cardiff Cwtchers",
-    "Cardiff Cwtchers": "Bristol Bats",
     "Manchester Monsters": "Yorkshire Puddings",
-    "Yorkshire Puddings": "Manchester Monsters",
     "Dublin Dodos": "Belfast Buccaneers",
-    "Belfast Buccaneers": "Dublin Dodos"
 }
 
 def get_rivals(team):
     return local_rivals[team]
 
-def rotate(arr):
-    return np.concatenate((arr[1:], [arr[0]]))
+def rotate(teams):
+    top_line = teams[0]
+    bottom_line = teams[1]
+    fixed_game = top_line.pop(0)
+    game_to_move = bottom_line.pop(0)
+    top_line.insert(0, game_to_move)
+    top_line.insert(0, fixed_game)
+    game_to_move = top_line.pop(-1)
+    bottom_line.append(game_to_move)
+    teams = [top_line, bottom_line]
+    return teams
 
 def update_league_table(outcome, round_no, match_no):
     result = outcome[0]
@@ -46,52 +49,41 @@ def update_league_table(outcome, round_no, match_no):
         loser.points += 1
     pass
 
-def createBracket():
+def createBracket(team_list):
 
-    seed = np.arange(1, 13)
-    shuffle(seed)
 
-    seed_dict = dict(sorted(zip(seed, team_list)))
-
+    # print(team_list)
+    shuffle(team_list)
+    top_line = team_list[:6]
+    bottom_line = team_list[6:]
+    teams = [top_line, bottom_line]
     N_rounds = 11
     N_matches = 6
-    top_line = np.arange(0, 6)
-    bottom_line = np.arange(6, 12)[::-1]
-    fixed_i = 0
-    games_i = np.concatenate((top_line[1:], bottom_line))
     rounds = {}
     for round_no in range(1, N_rounds+1):
         print(f"Round {round_no}")
-        if round_no % 2 == 0:
-            home_i = np.concatenate(([0], games_i[:5]))
-            away_i = np.array(games_i[5:])
-        else:
-            away_i = np.concatenate(([0], games_i[:5]))
-            home_i = np.array(games_i[5:])
-        home_teams = [seed_dict[i+1] for i in home_i]
-        away_teams = [seed_dict[i+1] for i in away_i]
         matches = {}
-        for i, match_no in enumerate(range(1, N_matches+1)):
+        for match_no in range(1, N_matches+1):
             print(f"\tMatch {match_no}")
-            print(f"\t\t{home_teams[i]} vs {away_teams[i]}")
-            matches[f"Match {match_no}"] = (home_teams[i], away_teams[i])
+            print(f"\t\t{teams[0][match_no-1]} vs {teams[1][match_no-1]}")
+            matches[f"Match {match_no}"] = (teams[0][match_no-1], teams[1][match_no-1])
         rounds[f"Round {round_no}"] = matches
-        games_i = rotate(games_i)
-
-    home_teams = [seed_dict[i+1] for i in top_line]
+        teams = rotate(teams)
+    
+    home_teams = list(local_rivals.keys())
     away_teams = []
     for team in home_teams:
         rival = get_rivals(team)
         away_teams.append(rival)
-    print(f"Local Derby")
+    local_matches = {}
     for i, match_no in enumerate(range(1, N_matches+1)):
         print(f"\tMatch {match_no}")
         print(f"\t\t{home_teams[i]} vs {away_teams[i]}")
-        matches[f"Match {match_no}"] = (home_teams[i], away_teams[i])
-    rounds["Local Derby"] = matches
-
+        local_matches[f"Match {match_no}"] = (home_teams[i], away_teams[i])
+    rounds["Local Derby"] = local_matches
+    print(rounds["Round 11"])
     with open('../assets/fixtures.json', 'w') as f:
         json.dump(rounds, f, indent=4)
     
 if __name__ == "__main__":
-    createBracket()
+    createBracket(team_list)
