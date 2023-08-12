c29925540a06db8a19920ada0b763531371f0e57
Tom Joshi-Cale <tj294@exeter.ac.uk>
2023-08-12 14:05:48 +0100

    Replaced Irish teams with Devon and Cornwall

--- API/league.py
+++ API/league.py
@@ -4,8 +4,8 @@ from random import shuffle
 import json
 
 team_list = []
-for file in glob('../Teams/*'):
-    team = file.split('/')[-1].replace('_', ' ')
+for file in glob("../Teams/*"):
+    team = file.split("/")[-1].replace("_", " ")
     team_list.append(team)
 
 local_rivals = {
@@ -14,11 +14,13 @@ local_rivals = {
     "Edinburgh XI": "Glasgow Goofballs",
     "Bristol Bats": "Cardiff Cwtchers",
     "Manchester Monsters": "Yorkshire Puddings",
-    "Dublin Dodos": "Belfast Buccaneers",
+    "Devon Devils": "Cornwall Catastophes",
 }
+
 
 def get_rivals(team):
     return local_rivals[team]
+
 
 def rotate(teams):
     top_line = teams[0]
@@ -32,11 +34,12 @@ def rotate(teams):
     teams = [top_line, bottom_line]
     return teams
 
+
 def update_league_table(outcome, round_no, match_no):
     result = outcome[0]
     winner = outcome[1]
     loser = outcome[2]
-    if result=='win':
+    if result == "win":
         winner.gamesPlayed += 1
         loser.gamesPlayed += 1
         winner.gamesWon += 1
@@ -49,9 +52,8 @@ def update_league_table(outcome, round_no, match_no):
         loser.points += 1
     pass
 
+
 def createBracket(team_list):
-
-
     # print(team_list)
     shuffle(team_list)
     top_line = team_list[:6]
@@ -60,30 +62,34 @@ def createBracket(team_list):
     N_rounds = 11
     N_matches = 6
     rounds = {}
-    for round_no in range(1, N_rounds+1):
+    for round_no in range(1, N_rounds + 1):
         print(f"Round {round_no}")
         matches = {}
-        for match_no in range(1, N_matches+1):
+        for match_no in range(1, N_matches + 1):
             print(f"\tMatch {match_no}")
             print(f"\t\t{teams[0][match_no-1]} vs {teams[1][match_no-1]}")
-            matches[f"Match {match_no}"] = (teams[0][match_no-1], teams[1][match_no-1])
+            matches[f"Match {match_no}"] = (
+                teams[0][match_no - 1],
+                teams[1][match_no - 1],
+            )
         rounds[f"Round {round_no}"] = matches
         teams = rotate(teams)
-    
+
     home_teams = list(local_rivals.keys())
     away_teams = []
     for team in home_teams:
         rival = get_rivals(team)
         away_teams.append(rival)
     local_matches = {}
-    for i, match_no in enumerate(range(1, N_matches+1)):
+    for i, match_no in enumerate(range(1, N_matches + 1)):
         print(f"\tMatch {match_no}")
         print(f"\t\t{home_teams[i]} vs {away_teams[i]}")
         local_matches[f"Match {match_no}"] = (home_teams[i], away_teams[i])
     rounds["Local Derby"] = local_matches
     print(rounds["Round 11"])
-    with open('../assets/fixtures.json', 'w') as f:
+    with open("../assets/fixtures.json", "w") as f:
         json.dump(rounds, f, indent=4)
-    
+
+
 if __name__ == "__main__":
     createBracket(team_list)
--- API/player_gen.py
+++ API/player_gen.py
@@ -9,15 +9,28 @@ from scipy import stats
 
 from classes import Player, generate_stat, pdf
 
-N_FIRST_NAMES = 1188
-N_LAST_NAMES = 1180
+N_FIRST_NAMES = 1191
+N_LAST_NAMES = 1183
 
 remove = True
 plot = False
 
-team_list = ['Parliamentary Penpushers' , 'Brighton Beachcombers', 'Birmingham Bullfrogs', 'Nottingham Nightingales', 'Edinburgh XI', 'Glasgow Goofballs', 'Bristol Bats', 'Cardiff Cwtchers', 'Manchester Monsters', 'Yorkshire Puddings', 'Dublin Dodos','Belfast Buccaneers']
+team_list = [
+    "Parliamentary Penpushers",
+    "Brighton Beachcombers",
+    "Birmingham Bullfrogs",
+    "Nottingham Nightingales",
+    "Edinburgh XI",
+    "Glasgow Goofballs",
+    "Bristol Bats",
+    "Cardiff Cwtchers",
+    "Manchester Monsters",
+    "Yorkshire Puddings",
+    "Devon Devils",
+    "Cornwall Catastrophes",
+]
 if remove:
-    teams = glob('../Teams/*')
+    teams = glob("../Teams/*")
     for team in teams:
         shutil.rmtree(team)
 
@@ -25,18 +38,18 @@ if remove:
 def generate_player(team):
     first_name_no = npr.randint(0, N_FIRST_NAMES)
     last_name_no = npr.randint(0, N_LAST_NAMES)
-    first_names = np.loadtxt('../assets/first_names.txt', dtype=str)
-    last_names = np.loadtxt('../assets/last_names.txt', dtype=str)
+    first_names = np.loadtxt("../assets/first_names.txt", dtype=str)
+    last_names = np.loadtxt("../assets/last_names.txt", dtype=str)
     fname = first_names[first_name_no]
     lname = last_names[last_name_no]
-    return Player(fname, lname, team)    
+    return Player(fname, lname, team)
 
 
 for team in team_list:
     print(team)
     players = []
     means = []
-    os.makedirs('../Teams/' + team.replace(' ', '_') + '/players/')
+    os.makedirs("../Teams/" + team.replace(" ", "_") + "/players/")
     for i in range(11):
         p = generate_player(team)
         players.append(p)
@@ -46,10 +59,13 @@ for team in team_list:
     keep_i = npr.randint(0, 11)
     players[keep_i].isKeeper = True
     for p in players:
-        print('\t', end='')
-        with open('../Teams/' + team.replace(' ', '_') + f'/players/{p.fname}{p.lname}.json', 'a+') as f:
+        print("\t", end="")
+        with open(
+            "../Teams/" + team.replace(" ", "_") + f"/players/{p.fname}{p.lname}.json",
+            "a+",
+        ) as f:
             json.dump(p.full(), f, indent=4)
         print(p)
         if plot:
             p.plot_stats()
-    print('\n')
+    print("\n")
